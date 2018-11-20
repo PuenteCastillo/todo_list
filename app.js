071059087d4c451eeeee4566e8584cfe02459b4a
The Chosen one <Josepuente2013@gmail.com>
2018-11-19 17:31:43 -0800

    adding files

---
+++ app.js
@@ -0,0 +1,36 @@
+
+$("button").click(addTask);
+
+
+$(document).on("click", "#delete", removeTask);
+
+
+$("input").keypress(function(event) {
+
+ 
+  if (event.which === 13) {
+
+    
+    addTask();
+  }
+});
+
+
+function addTask() {
+
+
+  var task = $("#new-task").val();
+
+  
+  $("#tasks").append("<div>" + task + "<span id='delete'>X</span></div>");
+
+  
+  $("#new-task").val("");
+}
+
+
+function removeTask() {
+
+ 
+  $(this).closest("div").remove();
+}
